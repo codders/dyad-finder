f24b2d6b1a620039ee52334797b05b738329f6eb
Arthur Taylor <arthur.taylor@gmail.com>
2020-04-30 13:26:46 +0200

    Refactored to be an express app Added API calls for collecting preferences and adding / deleting groups

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,21 +3,72 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 
+import * as express from 'express';
+
 admin.initializeApp();
 
-export const postPreference = functions.region('europe-west1').https.onRequest((request, response) => {
-  const postData = Object.assign({}, request.body);
+const app = express();
+
+app.post('/group/:id', (req, res) => {
+  const postData = Object.assign({}, req.body);
   console.log("Got postData", postData);
-  admin.firestore().collection('users').doc(postData.me).set({
+  admin.firestore().collection('groups').doc(req.params.id).set({
+    members: postData.members
+  })
+    .then(doc => {
+      console.log("Created group", doc);
+      res.send("Success");
+    })
+    .catch(error => {
+      console.log("Unable to create group", error);
+      res.send("Error");
+    });
+});
+
+app.delete('/group/:id', (req, res) => {
+  console.log("Deleting group", req.params.id);
+  admin.firestore().collection('groups').doc(req.params.id).collection('preferences').get()
+    .then((snapshot) => {
+      if (snapshot.size === 0) {
+        return 0;
+      }
+
+      const batch = admin.firestore().batch();
+      snapshot.docs.forEach((doc) => {
+        batch.delete(doc.ref);
+      });
+
+      return batch.commit().then(() => {
+        return snapshot.size;
+      });
+    })
+    .then(deleted => {
+      return admin.firestore().collection('groups').doc(req.params.id).delete()
+        .then(ref => {
+          res.send("Deleted: " + deleted);
+        });
+    })
+    .catch(error => {
+      console.log("error deleting group " + req.params.id, error);
+      res.send("Error");
+    });
+});
+
+app.post('/group/:id/preference', (req, res) => {
+  const postData = Object.assign({}, req.body);
+  console.log("Got postData", postData);
+  admin.firestore().collection('groups').doc(req.params.id).collection('preferences').doc(postData.me).set({
     'preferences': postData.preferences,
     'me': postData.me
   })
     .then(function(doc) {
       console.log("Saved doc", doc);
-      response.send("Success\n\n");
+      res.send("Success\n\n");
     })
     .catch(function(error) {
       console.log("Unable to save doc", error);
-      response.send("Error\n\n");
+      res.send("Error\n\n");
     });
 });
+
+exports.dyad = functions.region('europe-west1').https.onRequest(app);
